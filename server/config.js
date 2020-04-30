2241e7937c9c75c2db00ebb558ee9bb43e55aa39
ngjamesng <just.james.ng@gmail.com>
2020-04-30 11:12:58 -0700

    add db configuration

---
+++ server/config.js
@@ -0,0 +1,30 @@
+/** Shared config for application; can be req'd many places. */
+
+
+require("dotenv").config();
+
+const SECRET = process.env.SECRET_KEY || 'test';
+
+const PORT = +process.env.PORT || 3000;
+
+// database is:
+//
+// - on Heroku, get from env var DATABASE_URL
+// - in testing, 'weatherwear-test'
+// - else: 'weatherwear'
+
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+  DB_URI = "weatherwear-test";
+} else {
+  DB_URI  = process.env.DATABASE_URL || 'weatherwear';
+}
+
+console.log("Using database", DB_URI);
+
+module.exports = {
+  SECRET,
+  PORT,
+  DB_URI,
+};
