c65d0a00605f699bd2b546223b8e88225ea284ae
ngjamesng <just.james.ng@gmail.com>
2020-04-29 23:48:26 -0700

    add starter db schema

---
+++ server/data.sql
@@ -0,0 +1,19 @@
+\c weatherwear 
+
+DROP TABLE IF EXISTS conditions;
+DROP TABLE IF EXISTS locations;
+
+CREATE TABLE locations (
+    WOEID integer PRIMARY KEY,
+    city_name text NOT NULL,
+    location_type text NOT NULL
+);
+
+CREATE TABLE conditions (
+  id SERIAL PRIMARY KEY,
+  WOEID integer REFERENCES locations(WOEID),
+  applicable_date date, 
+  the_temp integer, 
+  wind_speed float,
+  weather_state_name text NOT NULL
+);
