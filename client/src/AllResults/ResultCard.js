f69d29f0a47ee2f365ae49a7f3d3010c0a5cd7a4
ngjamesng <just.james.ng@gmail.com>
2020-05-01 22:14:46 -0700

    add Card, fix link bug

--- client/src/AllResults/ResultCard.js
+++ client/src/AllResults/ResultCard.js
@@ -1,29 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Media } from "react-bootstrap";
+import { Media, Card } from "react-bootstrap";
 import moment from "moment";
 const METAWEATHER_IMG_URL = abbr => `https://www.metaweather.com/static/img/weather/${abbr}.svg`;
 
 function ResultCard({ data }) {
   return (
-    <Media>
-      <img
-        width={32}
-        height={32}
-        className="mr-3"
-        src={METAWEATHER_IMG_URL(data.weather_state_abbr)}
-        alt={data.weather_state_name}
-      />
-      <Media.Body>
-        <Link to={`results/${data.id}`}>
-          <h5>{moment(data.applicable_date).format("LL")} in {data.city_name} </h5>
-        </Link>
-        <p>Temperature: {data.the_temp} degrees {"Celsius"} |
+    <Card>
+      <Media>
+        <img
+          width={32}
+          height={32}
+          className="mr-3"
+          src={METAWEATHER_IMG_URL(data.weather_state_abbr)}
+          alt={data.weather_state_name}
+        />
+        <Media.Body>
+          <h5>
+            <Link to={`results/${data.id}`}>
+              {moment(data.applicable_date).format("LL")} in {data.city_name}
+            </Link>
+          </h5>
+          <p>Temperature: {data.the_temp} degrees {"Celsius"} |
           Wind speed: {data.wind_speed} mph | {" "}
-          {data.weather_state_name}
-        </p>
-      </Media.Body>
-    </Media>)
+            {data.weather_state_name}
+          </p>
+        </Media.Body>
+      </Media>
+    </Card>
+  )
 }
 
 export default ResultCard;
