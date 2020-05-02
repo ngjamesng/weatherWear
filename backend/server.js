270e88a8f9ebedfed506e0553d4100664063348d
ngjamesng <just.james.ng@gmail.com>
2020-05-02 16:54:22 -0700

    ready to deploy backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const { PORT } = require("./config");
 
-app.listen(port, () => {
-  console.log(`Server listening on ${port}`)
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`)
 });
