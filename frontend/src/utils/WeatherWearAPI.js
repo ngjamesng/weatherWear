9e5ec80b788e80756075c5f76fe7ea842386f8d4
ngjamesng <just.james.ng@gmail.com>
2020-05-02 20:22:03 -0700

    fix localhost:3000 bug

--- frontend/src/utils/WeatherWearAPI.js
+++ frontend/src/utils/WeatherWearAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3000";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3000/";
 
 class WeatherWearAPI {
   static async request(endpoint, params = {}, verb = "get") {
