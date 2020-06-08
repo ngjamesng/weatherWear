69cc418b08c3deb4f5e3efc174436c3060784e8d
ngjamesng <just.james.ng@gmail.com>
2020-06-08 13:41:08 -0700

    add get by coordinates

--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -1,28 +1,39 @@
 const express = require("express");
 const router = express.Router();
 // const LocationConditions = require("../models/locationConditions");
-const { getByNameOrZip } = require("../utils/openWeatherAPI");
+const { getByNameOrZip, getByCoordinates } = require("../utils/openWeatherAPI");
 
-/** handle submission form from fronend */
-router.get("/", async (req, res, next) => {
+/** handle submission form from frontend */
+router.post("/", async (req, res, next) => {
   try {
-    const { cityOrZip } = req.body;
-
-    const data = await getByNameOrZip(cityOrZip)
+    const { city, zip } = req.body;
+    if (!city && !zip) return next();
+    const data = await getByNameOrZip({ city, zip });
     return res.json(data);
   } catch (err) {
     return next(err);
   }
 });
 
-router.get("/:id", async (req, res, next) => {
+router.get("/coordinates/", async (req, res, next) => {
   try {
-    const { id } = req.params;
-    let condition = await LocationConditions.getConditionById(id);
-    return res.json(condition[0]);
+    const { lon, lat } = req.body;
+    if (!lon || !lat) return next();
+    const data = await getByCoordinates({lon, lat});
+    return res.json(data);
   } catch (err) {
     return next(err);
   }
-});
+})
+
+// router.get("/:id", async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     let condition = await LocationConditions.getConditionById(id);
+//     return res.json(condition[0]);
+//   } catch (err) {
+//     return next(err);
+//   }
+// });
 
 module.exports = router;
--- backend/utils/openWeatherAPI.js
+++ backend/utils/openWeatherAPI.js
@@ -1,15 +1,14 @@
 const axios = require('axios');
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
-async function getByNameOrZip(cityOrZip) {
-  const key = process.env.OPEN_WEATHER_API_KEY
+async function getByNameOrZip({ city, zip }) {
+  const key = process.env.OPEN_WEATHER_API_KEY;
   try {
-
+    const cityOrZip = city ? { q: city } : { zip: zip };
     let resp = await axios.get(`${BASE_URL}weather`, {
       params:
       {
-        zip: cityOrZip,
-        q: cityOrZip,
+        ...cityOrZip,
         units: "metric",
         appid: key
       }
@@ -20,7 +19,24 @@ async function getByNameOrZip(cityOrZip) {
   }
 }
 
+async function getByCoordinates({ lon, lat }) {
+  const key = process.env.OPEN_WEATHER_API_KEY;
+  try {
+    let resp = await axios.get(`${BASE_URL}weather`, {
+      params: {
+        lon,
+        lat,
+        units: "metric",
+        appid: key
+      }
+    })
+    return resp.data
+  } catch (err) {
+    console.error("error:", err);
+  }
+}
 
 module.exports = {
-  getByNameOrZip
+  getByNameOrZip,
+  getByCoordinates
 };
