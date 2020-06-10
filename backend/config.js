2126be8a59baec3f0c63c2a4a0c5bf97e270e6a9
ngjamesng <just.james.ng@gmail.com>
2020-06-09 18:24:25 -0700

    update database URI

--- backend/config.js
+++ backend/config.js
@@ -16,9 +16,9 @@ const PORT = +process.env.PORT || 3000;
 let DB_URI;
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = "weatherwear-test";
+  DB_URI = "weatherwear-openweather-test";
 } else {
-  DB_URI  = process.env.DATABASE_URL || 'weatherwear';
+  DB_URI  = process.env.DATABASE_URL || 'weatherwear-openweather';
 }
 
 console.log("Using database", DB_URI);
