26d927d280f06881a86cf43cf00b625a63b71b48
ngjamesng <just.james.ng@gmail.com>
2020-07-04 23:21:24 -0700

    refactor component loading

--- frontend/src/Result/index.js
+++ frontend/src/Result/index.js
@@ -21,33 +21,33 @@ function Result({ resultData: data, isLoading }) {
     <Container className="mt-5">
       <Media>
         {!data || isLoading
-          ? <Skeleton height={64} width={64} circle />
+          ? <>
+            <Skeleton height={64} width={64} circle />
+            <Media.Body>
+              <Skeleton height={30} />
+              <Skeleton count={4} />
+            </Media.Body>
+          </>
           :
-          <img width={64} height={64} className="mr-3"
-            src={OPEN_WEATHER_IMG_URL(data.weather[0].icon)}
-            alt={data.weather[0].description}
-          />
-        }
-        {!data || isLoading
-          ?
-          <Media.Body>
-            <Skeleton height={30} />
-            <Skeleton count={4} />
-          </Media.Body>
-          :
-          <Media.Body>
-            <h5>The weather in {data?.name}</h5>
-            <p>
-              Here's the weather on {moment.unix(data?.dt + data?.timezone).utc().format("LLL")}. <br />
+          <>
+            <img width={64} height={64} className="mr-3"
+              src={OPEN_WEATHER_IMG_URL(data.weather[0].icon)}
+              alt={data.weather[0].description}
+            />
+            <Media.Body>
+              <h5>The weather in {data?.name}</h5>
+              <p>
+                Here's the weather on {moment.unix(data?.dt + data?.timezone).utc().format("LLL")}. <br />
             temperature: {(data?.main.temp) && displayTemp(tempPreference, data?.main.temp)}° {displayMeasurement(tempPreference)}. <br />
             wind speed: {(data?.wind.speed * 2.23694).toFixed(1)} mph.<br />
             conditions: {data?.weather[0].description}.<br />
             cloudiness: {data?.clouds.all}% <br />
-              {data?.rain && `rain volume for the last hour: ${data?.rain["1h"]} mm ${<br />}`}
-              {data?.snow && `snow volume for the last hour: ${data?.snow["1h"]} mm ${<br />}`}
+                {data?.rain && `rain volume for the last hour: ${data?.rain["1h"]} mm ${<br />}`}
+                {data?.snow && `snow volume for the last hour: ${data?.snow["1h"]} mm ${<br />}`}
             humidity: {data?.main.humidity}%
           </p>
-          </Media.Body>
+            </Media.Body>
+          </>
         }
       </Media>
       {data && !isLoading && <Recommendation data={data} />}
