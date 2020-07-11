6ff31eae1c456048e58e1f58308e1fed84b9ba76
ngjamesng <just.james.ng@gmail.com>
2020-07-10 20:22:27 -0700

    save preferences to localStorage

--- frontend/src/Preferences/rootReducer.js
+++ frontend/src/Preferences/rootReducer.js
@@ -5,36 +5,43 @@ import {
   CHANGE_TO_HIGH_ACTIVITY
 } from "./actionTypes";
 
-const INITIAL_STATE = {
+const INITIAL_STATE = JSON.parse(localStorage.getItem("preferences")) || {
   temperaturePreference: "celsius",
   activityLevel: "low"
 }
 
 function rootReducer(state = INITIAL_STATE, action) {
+  let changedState = {};
   switch (action.type) {
     case CHANGE_TO_FAHRENHEIT:
-      return {
+      changedState = {
         ...state,
         temperaturePreference: action.payload.temperaturePreference
-      }
+      };
+      break;
     case CHANGE_TO_CELSIUS:
-      return {
+      changedState = {
         ...state,
         temperaturePreference: action.payload.temperaturePreference
-      }
+      };
+      break;
     case CHANGE_TO_LOW_ACTIVITY:
-      return {
+      changedState = {
         ...state,
         activityLevel: action.payload.activityLevel
-      }
+      };
+      break;
     case CHANGE_TO_HIGH_ACTIVITY:
-      return {
+      changedState = {
         ...state,
         activityLevel: action.payload.activityLevel
-      }
+      };
+      break;
     default:
       return state;
   }
+  localStorage.setItem("preferences", JSON.stringify(changedState));
+  return changedState;
 }
 
 export default rootReducer;
